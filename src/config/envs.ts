5af228901d9619a40ab9673ae8e0b0636dfcec1e
Keneth <kenethcubas2002@unitec.edu>
2026-02-09 19:16:12 -0600

    refactor: remove unused microservice environment variables and integrate NATS_SERVERS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,25 +2,21 @@ import 'dotenv/config';
 import * as joi from 'joi';
 
 interface EnvVars {
-  PORT: number;
-  PRODUCTS_MICROSERVICE_HOST: string;
-  PRODUCTS_MICROSERVICE_PORT: number;
-  ORDERS_MICROSERVICE_HOST: string;
-  ORDERS_MICROSERVICE_PORT: number;
-  // DATABASE_URL: string;
+  PORT: number;  
+  NATS_SERVERS: string;
 }
+
 const envVarsSchema = joi
   .object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
-    ORDERS_MICROSERVICE_HOST: joi.string().required(),
-    ORDERS_MICROSERVICE_PORT: joi.number().required(),
-    // DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -29,10 +25,6 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
-  port: envVars.PORT,
-  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
-  ordersMicroserviceHost: envVars.ORDERS_MICROSERVICE_HOST,
-  ordersMicroservicePort: envVars.ORDERS_MICROSERVICE_PORT,
-  // databaseUrl: envVars.DATABASE_URL
+  port: envVars.PORT,  
+  natsServers: envVars.NATS_SERVERS,
 };
