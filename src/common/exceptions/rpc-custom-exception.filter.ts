a65d85edd8dfc7b37a5e550cd01e5145fb1b139f
Keneth <kenethcubas2002@unitec.edu>
2026-02-11 17:16:46 -0600

    fix: enhance RpcCustomExceptionFilter to handle empty response errors

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,5 +1,4 @@
 import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
 
 @Catch(RpcException)
@@ -10,12 +9,21 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
 
     const rpcError = exception.getError();
 
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(500).json({
+        status: 500,
+        message: rpcError.toString().substring(0, rpcError.toString().indexOf('(') -1)
+      })
+    }
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
       'message' in rpcError
     ) {
-      const status = isNaN(+(rpcError as any).status) ? 400 : +(rpcError as any).status;
+      const status = isNaN(+(rpcError as any).status)
+        ? 400
+        : +(rpcError as any).status;
       return response.status(status).json(rpcError);
     }
 
