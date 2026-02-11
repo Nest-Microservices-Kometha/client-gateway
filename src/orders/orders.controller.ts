cf13ed664565537c3592c0ac0e5f34617d1a7ae3
Keneth <kenethcubas2002@unitec.edu>
2026-02-11 17:16:52 -0600

    fix: update findOrders method to handle asynchronous response and errors

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -21,8 +21,15 @@ export class OrdersController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Get()
-  findOrders(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.client.send('findAllOrders', orderPaginationDto);
+  async findOrders(@Query() orderPaginationDto: OrderPaginationDto) {
+    try {
+      const orders = await firstValueFrom(
+        this.client.send('findAllOrders', orderPaginationDto),
+      );
+      return orders;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get(':status')
